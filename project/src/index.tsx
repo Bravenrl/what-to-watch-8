8a08b6a7bccf17fe1e0b618b9ee9c93315b5c076
Bravenrl <bravenrl@gmail.com>
2021-12-11 02:07:26 +0300

    add BrowserHistory

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HistoryRouter } from 'react-router-dom';
 import App from './components/app/app';
 import { checkAuthStatus } from './store/api-actions';
 import { store } from './store/store';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
+import browserHistory from './browser-history';
 
 store.dispatch(checkAuthStatus());
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HistoryRouter history={browserHistory}>
         <ToastContainer />
         <App />
-      </BrowserRouter>
+      </HistoryRouter>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root'),
-);
+  document.getElementById('root'));
